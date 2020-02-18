8eaaab06d2ebf798c28bcfeac0326d938c1a1d54
soumik12345 <19soumik.rakshit96@gmail.com>
2020-02-18 19:27:09 +0530

    Added Notebook

--- src/dataset.py
+++ src/dataset.py
@@ -8,12 +8,14 @@ def download_dataset(tfds_address, disable_progress_bar=False):
     return train_data, val_data
 
 
-def get_tokenizers(dataset):
+def get_tokenizers(dataset, approx_vocab_size=2 ** 13):
     source_tokenizer = tfds.features.text.SubwordTextEncoder.build_from_corpus(
         (i.numpy() for i, j in dataset),
-        target_vocab_size=2 ** 13
+        target_vocab_size=approx_vocab_size
     )
     target_tokenizer = tfds.features.text.SubwordTextEncoder.build_from_corpus(
         (j.numpy() for i, j in dataset),
-        target_vocab_size=2 ** 13
+        target_vocab_size=approx_vocab_size
     )
+    return source_tokenizer, target_tokenizer
+
