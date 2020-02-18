c131744212b420fcb0c3438ab013c150ce997315
soumik12345 <19soumik.rakshit96@gmail.com>
2020-02-18 18:31:34 +0530

    Added source

---
+++ src/dataset.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+
+def download_dataset(tfds_address, disable_progress_bar=False):
+    data, metadata = tfds.load(tfds_address, with_info=True, as_supervised=True)
+    train_data, val_data = data['train'], data['validation']
+    return train_data, val_data
+
+
+def get_tokenizers(dataset):
+    source_tokenizer = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+        (i.numpy() for i, j in dataset),
+        target_vocab_size=2 ** 13
+    )
+    target_tokenizer = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+        (j.numpy() for i, j in dataset),
+        target_vocab_size=2 ** 13
+    )
