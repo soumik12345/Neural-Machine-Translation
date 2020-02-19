0ad817b258b131822c0b94097f3c586cb9044515
soumik12345 <19soumik.rakshit96@gmail.com>
2020-02-19 12:01:02 +0530

    Added Encoder

--- src/layers.py
+++ src/layers.py
@@ -1,3 +1,4 @@
+from .utils import *
 from .blocks import *
 import tensorflow as tf
 
@@ -21,3 +22,34 @@ class EncoderLayer(tf.keras.layers.Layer):
         ffn_out = self.dropout_2(ffn_out, training=training)
         output_2 = self.norm_2(output_1 + ffn_out)
         return output_2
+
+
+
+class Encoder(tf.keras.layers.Layer):
+
+    def __init__(
+        self, n_encoder_layers, dense_units,
+        attention_heads, dff, input_vocab_size,
+        max_positional_encoding, dropout_rate=0.1):
+        super(Encoder, self).__init__()
+        self.dense_units = dense_units
+        self.n_encoder_layers = n_encoder_layers
+        self.embedding = tf.keras.layers.Embedding(input_vocab_size, dense_units)
+        self.positional_encoding = positional_encoding(max_positional_encoding, self.dense_units)
+        self.encoder_layers = [
+            EncoderLayer(
+                self.dense_units, attention_heads,
+                dff, dropout_rate
+            ) for _ in range(self.n_encoder_layers)
+        ]
+        self.dropout = tf.keras.layers.Dropout(dropout_rate)
+    
+    def call(self, x, training, mask):
+        sequence_length = tf.shape(x)[1]
+        x = self.embedding(x)
+        x *= tf.math.sqrt(tf.cast(self.dense_units, dtype=tf.float32))
+        x += self.positional_encoding[:, :sequence_length, :]
+        x = self.dropout(x, training=training)
+        for i in range(self.n_encoder_layers):
+            x = self.encoder_layers[i](x, training, mask)
+        return x
