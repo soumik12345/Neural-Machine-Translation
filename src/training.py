c9c31a82f97882f2088bd1304dc269812bde6cf2
soumik12345 <19soumik.rakshit96@gmail.com>
2020-02-20 02:25:33 +0530

    Updated LR Scheduler

--- src/training.py
+++ src/training.py
@@ -3,11 +3,12 @@ import tensorflow as tf
 
 
 class CustomLRScheduler(tf.keras.optimizers.schedules.LearningRateSchedule):
-
+    
     def __init__(self, dense_units, warmup_steps=4000):
         super(CustomLRScheduler, self).__init__()
-        self.dense_units = tf.cast(dense_units, dtype=tf.float32)
+        self.dense_units = dense_units
+        self.dense_units = tf.cast(self.dense_units, tf.float32)
         self.warmup_steps = warmup_steps
 
     def __call__(self, step):
-       return tf.math.rsqrt(self.dense_units) + tf.math.minimum(tf.math.rsqrt(step), self.warmup_steps ** -1.5)
+        return tf.math.rsqrt(self.dense_units) * tf.math.minimum(tf.math.rsqrt(step), step * (self.warmup_steps ** -1.5))
