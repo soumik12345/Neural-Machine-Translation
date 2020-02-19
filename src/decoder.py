d7211dbc457460c466dc09134e5513b7f8898f78
soumik12345 <19soumik.rakshit96@gmail.com>
2020-02-19 15:22:35 +0530

    Added Decoder

--- src/decoder.py
+++ src/decoder.py
@@ -31,3 +31,39 @@ class DecoderLayer(tf.keras.layers.Layer):
         ffn_out = self.dropout_3(ffn_out, training=training)
         out_3 = self.norm_3(ffn_out + out_2)
         return out_3, attn_weights_1, attn_weights_2
+
+
+class Decoder(tf.keras.layers.Layer):
+
+    def __init__(
+            self, n_decoder_layers, dense_units,
+            attention_heads, dff, target_vocab_size,
+            max_position_encoding, dropout_rate=0.1):
+        super(Decoder, self).__init__()
+        self.dense_units = dense_units
+        self.n_decoder_layers = n_decoder_layers
+        self.embedding = tf.keras.layers.Embedding(target_vocab_size, dense_units)
+        self.pos_encoding = positional_encoding(max_position_encoding, dense_units)
+        self.decoder_layers = [
+            DecoderLayer(
+                dense_units, attention_heads,
+                dff, dropout_rate
+            ) for _ in range(n_decoder_layers)
+        ]
+        self.dropout = tf.keras.layers.Dropout(dropout_rate)
+
+    def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
+        sequence_length = tf.shape(x)[1]
+        attention_weights = {}
+        x = self.embedding(x)
+        x *= tf.math.sqrt(tf.cast(self.dense_units, dtype=tf.float32))
+        x += self.pos_encoding[:, :sequence_length, :]
+        x = self.dropout(x, training=training)
+        for i in range(self.n_decoder_layers):
+            x, block_1, block_2 = self.decoder_layers[i](
+                x, encoder_output, training,
+                look_ahead_mask, padding_mask
+            )
+            attention_weights['decoder_layer_{}_block_1'.format(i + 1)] = block_1
+            attention_weights['decoder_layer_{}_block_2'.format(i + 1)] = block_2
+        return x, attention_weights
